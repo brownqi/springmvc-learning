c3aa70e322361a16408ba350b477908de285e9de
BrownQi <brownqi@foxmail.com>
2020-04-10 22:48:51 +0800

    springmvc-02-annotation 注解配置

---
+++ springmvc-02-annotation/src/main/java/cn/brownqi/controller/HelloController.java
@@ -0,0 +1,19 @@
+package cn.brownqi.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/hello")
+public class HelloController {
+
+    @RequestMapping("/h1")
+    public String hello(Model model){
+        //封装数据
+        model.addAttribute("msg","HelloSpringMVC Annotation");
+
+        return "hello"; //会被视图解析器处理。
+    }
+
+}
