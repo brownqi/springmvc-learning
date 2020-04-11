e66148d6ac5577bb6dd4c090d56b8d18c2e62572
BrownQi <brownqi@foxmail.com>
2020-04-11 21:01:59 +0800

    Ajax

--- springmvc-05-ajax/src/main/java/cn/brownqi/controller/AjaxController.java
+++ springmvc-05-ajax/src/main/java/cn/brownqi/controller/AjaxController.java
@@ -3,6 +3,9 @@ package cn.brownqi.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 @RestController
 public class AjaxController {
 
@@ -10,6 +13,18 @@ public class AjaxController {
     public String test(){
 
         return "hello";
+    }
+
+    @RequestMapping("/a1")
+    public void a1(String name, HttpServletResponse response) throws IOException {
+
+        System.out.println("a1:param=>" + name);
+        if ("brownqi".equals(name)){
+            response.getWriter().println("true");
+        }else {
+            response.getWriter().println("false");
+        }
+
     }
 
 }
