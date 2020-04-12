dd560b2e0336ae643ea235b64bbdaa02237096ca
BrownQi <brownqi@foxmail.com>
2020-04-12 11:40:01 +0800

    Ajax

--- springmvc-05-ajax/src/main/java/cn/brownqi/controller/AjaxController.java
+++ springmvc-05-ajax/src/main/java/cn/brownqi/controller/AjaxController.java
@@ -1,10 +1,13 @@
 package cn.brownqi.controller;
 
+import cn.brownqi.pojo.User;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class AjaxController {
@@ -24,7 +27,19 @@ public class AjaxController {
         }else {
             response.getWriter().println("false");
         }
+    }
 
+    @RequestMapping("/a2")
+    public List<User> a2(){
+        ArrayList<User> userList = new ArrayList<>();
+        //添加数据
+        userList.add(new User("齐",1,"male"));
+        userList.add(new User("齐",2,"male"));
+        userList.add(new User("齐",3,"male"));
+        userList.add(new User("齐",4,"male"));
+        userList.add(new User("齐",5,"male"));
+
+        return userList;
     }
 
 }
